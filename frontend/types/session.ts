d4482d1f0ac551027a85c7fe69c454a81731274f
Nathan Palileo <nathanpee2006@gmail.com>
2026-07-19 17:19:55 +1200

    Renamed folder structure in components folder

--- frontend/types/session.ts
+++
@@ -1,9 +0,0 @@
-export type Session = {
-  id: number;
-  date: string;
-  paintShotPercentage: number;
-  midrangeShotPercentage: number;
-  threePointShotPercentage: number;
-  freeThrowShotPercentage: number;
-  overallShotPercentage: number;
-};
